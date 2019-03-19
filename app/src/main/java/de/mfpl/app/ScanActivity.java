3da2a346d2839cd490ef615362cb16b822265f76
sebastianknopf <43302817+sebastianknopf@users.noreply.github.com>
2019-03-19 19:19:18 +0100

    imported zxing-embedded as module and added some new features in ScanActivity

--- app/src/main/java/de/mfpl/app/ScanActivity.java
+++ app/src/main/java/de/mfpl/app/ScanActivity.java
@@ -2,18 +2,25 @@ package de.mfpl.app;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.content.res.ColorStateList;
 import android.databinding.DataBindingUtil;
+import android.hardware.Camera;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
 import com.journeyapps.barcodescanner.CaptureManager;
+
+import java.util.List;
 
 import de.mfpl.app.databinding.ActivityScanBinding;
 
@@ -25,6 +32,7 @@ public class ScanActivity extends AppCompatActivity{
 
     private ActivityScanBinding components;
     private CaptureManager captureManager;
+    private boolean isTorchOn = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +47,23 @@ public class ScanActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         this.components = DataBindingUtil.setContentView(this, R.layout.activity_scan);
 
+        //  set toolbar
+        this.setSupportActionBar(this.components.scanToolbar);
+        ActionBar supportActionBar = this.getSupportActionBar();
+        if(supportActionBar != null) {
+            supportActionBar.setTitle(this.getString(R.string.str_scan_activity_title));
+            supportActionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
         // init capture view
         this.captureManager = new CaptureManager(this, this.components.barcodeView);
         this.captureManager.initializeFromIntent(this.getIntent(), savedInstanceState);
         this.captureManager.decode();
+
+        // disable flash button
+        if(!this.hasTorchLight()) {
+            this.components.fabTorchToggle.hide();
+        }
     }
 
     @Override
@@ -81,6 +102,61 @@ public class ScanActivity extends AppCompatActivity{
         } else {
             this.checkPermissions();
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void fabTorchToggleClick(View view) {
+        int fabBackgroundColor, fabForegroundColor;
+
+        if(this.isTorchOn) {
+            this.isTorchOn = false;
+            this.components.barcodeView.setTorchOff();
+
+            fabBackgroundColor = ContextCompat.getColor(this, android.R.color.white);
+            fabForegroundColor = ContextCompat.getColor(this, android.R.color.darker_gray);
+        } else {
+            this.isTorchOn = true;
+            this.components.barcodeView.setTorchOn();
+
+            fabBackgroundColor = ContextCompat.getColor(this, android.R.color.darker_gray);
+            fabForegroundColor = ContextCompat.getColor(this, android.R.color.white);
+        }
+
+
+        this.components.fabTorchToggle.setBackgroundTintList(ColorStateList.valueOf(fabBackgroundColor));
+        this.components.fabTorchToggle.setColorFilter(fabForegroundColor);
+    }
+
+    private boolean hasTorchLight() {
+        Camera camera = Camera.open(0);
+        if(camera == null) {
+            return false;
+        }
+
+        Camera.Parameters parameters = camera.getParameters();
+        if(parameters.getFlashMode() == null) {
+            camera.release();
+            return false;
+        }
+
+        List<String> flashModes = parameters.getSupportedFlashModes();
+        if(flashModes == null || flashModes.isEmpty() || flashModes.size() == 1 && flashModes.get(0).equals(Camera.Parameters.FLASH_MODE_OFF)) {
+            camera.release();
+            return false;
+        }
+
+        camera.release();
+        return true;
     }
 
     private void checkPermissions() {
